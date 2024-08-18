fde9b4fc9b7bfff8fea8ede9161dd569b87f72c1
newmohib <@@mohib100@@>
2024-08-18 14:11:01 +0600

    initial commit

---
+++ products/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+//
+app.use(express.json());
+
+const port = 8003;
+
+//api
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log("Example app listening on port port!", port);
+});
+
+//Run app, then load http://localhost:port in a browser to see the output.
